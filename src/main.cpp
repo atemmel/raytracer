08f04bba27338e8de9a7307e965eadb2b17ebce3
atemmel <temmeladam@gmail.com>
2021-12-27 11:24:36 +0100

    Removed dead code

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,8 @@
 
 #include <iostream>
 
-auto rayHitsSphere(const Ray& ray, Sphere sphere) -> float {
-	auto oc = ray.origin - sphere.origin;
-	auto a = ray.direction.squaredNorm();
-	auto halfB = oc.dot(ray.direction);
-	auto c = oc.squaredNorm() - sphere.radius * sphere.radius;
-	auto discriminant = halfB * halfB - a * c;
-
-	if(discriminant < 0.f) {
-		return -0.f;
-	}
-	// extract step value for ray
-	return (-halfB - std::sqrt(discriminant)) / a;
-}
-
-/*
-auto rayColor(const Ray& ray) -> Vec3 {
-	auto step = rayHitsSphere(ray, {{0.f, 0.f, -1.f}, 0.5f});
-	if(step > 0.f) {
-		auto normal = (ray.at(step) - Vec3{0.f, 0.f, -1.f}).unit();
-		return 0.5f 
-			* Vec3{normal.x + 1.f, normal.y + 1.f, normal.z + 1.f};
-	}
-	auto unit = ray.direction.unit();
-	auto t = 0.5f * (unit.y + 1.f);
-	return (1.f - t) * Vec3{1.f, 1.f, 1.f} 
-		+ t * Vec3{0.5f, 0.7f, 1.f};
-}
-*/
-
 auto rayColor(const Ray& ray, const World& world) -> Vec3 {
-	RayHitData data;
+	RayHitData data{};
 	if(ray.hit(world, 0.f, Infinity, data)) {
 		return 0.5f * (data.normal + Vec3{1.f, 1.f, 1.f});
 	}
@@ -85,19 +56,6 @@ auto main() -> int {
 			image(x, y) = normalizeColor(color, samplesPerPixel);
 		}
 	}
-
-	/*
-	for(size_t x = 0; x < image.width; x++) {
-		for(size_t y = 0; y < image.height; y++) {
-			size_t yn = image.height - 1 - y;
-			const float r = float(x) / (image.width - 1);
-			const float g = float(yn) / (image.height - 1);
-			const float b = 0.25f;
-
-			image(x, y) = {r, g, b};
-		}
-	}
-	*/
 
 	image.writeToPpm("image.ppm");
 }
